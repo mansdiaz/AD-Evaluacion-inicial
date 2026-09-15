7bd98eb2faf437705278164dd7a924235da9822e
Manuel Sánchez <manuelsanchezinstitut@gmail.com>
2026-09-15 11:40:47 +0200

    Recorrer array adelante/atrás; actualizar config de NB
    Add a simple demo in ProgramacionBasica.main that prints an int array in forward and reverse order. Also update NetBeans project metadata: bump stylesheet CRC entries in nbproject/genfiles.properties and change platform.active from JDK_23 to Oracle_26 in nbproject/project.properties.

--- src/ad/evaluacioninicial/ProgramacionBasica.java
+++ src/ad/evaluacioninicial/ProgramacionBasica.java
@@ -14,7 +14,20 @@ public class ProgramacionBasica {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       int[] numeros = {5,3,1,0,7};
+       
+       for (int i = 0;i<numeros.length;i++){
+           System.out.println("Posición " + i + ": " + numeros[i]);
+       }
+       
+        System.out.println("\n" + "Al revés" + "\n");
+        
+        for (int i = numeros.length - 1;i>=0;i--){
+           System.out.println("Posición " + i + ": " + numeros[i]);
+       }
+    
     }
+    
+    
     
 }
