97c3f7944ed6a0aeaa60eb866bac34a699a352f1
Manuel Sánchez <manuelsanchezinstitut@gmail.com>
2026-09-17 10:16:38 +0200

    Try - Catch

--- src/ad/evaluacioninicial/ProgramacionExcepciones.java
+++ src/ad/evaluacioninicial/ProgramacionExcepciones.java
@@ -4,6 +4,7 @@
  */
 package ad.evaluacioninicial;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -13,18 +14,24 @@ import java.util.Scanner;
 public class ProgramacionExcepciones {
     public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
-    int a, b;
+    int a = 0, b = 0;
     double result;
+    try{
         System.out.println("Introduce el primer numero");
         a = sc.nextInt();
         sc.nextLine();
         System.out.println("Introduce el segundo número");
         b = sc.nextInt();
         sc.nextLine();
+    } catch (InputMismatchException e){
+        System.err.println("\n" + "No puedes introducir un valor que no sea un número" + "\n");
+    }
         
         result = a / b;
         
         System.out.println("Resultado: " + result);
+        
+        
     
             
     }
